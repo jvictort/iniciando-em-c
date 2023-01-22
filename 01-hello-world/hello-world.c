4bcc6a7d7ce55dfb9fcd16b6ffd9fc10884e1916
jvictort <jvteodorobrasil@gmail.com>
2023-01-22 12:30:49 -0300

    Hello world

---
+++ 01-hello-world/hello-world.c
@@ -0,0 +1,20 @@
+/*
+	 C é uma linguagem de programação compilada, estruturada e procedural de propósito geral.
+	Além disso, a linguagem C possibilita algumas features de baixo nível, como manipulação de bytes
+	e endereços de memória.
+*/
+
+// Definição de bibliotecas para a aplicação
+// stdio.h trata-se de um cabeçalho de uma biblioteca padrão da linguagem C responsável por operações de input e output de dados
+#include <stdio.h>
+
+// Criação da função main() que será executada assim que o programa for inicializado
+int main()
+{
+	// Instruções de main()
+	// A função printf() vem da biblioteca importada
+	printf("Hello world!\n");
+
+	// Retorno de um valor numérico (0 indica um processo bem sucedido)
+	return 0;
+}
