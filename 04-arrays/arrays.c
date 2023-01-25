fa546971ec30ddc184cbe4421b0f137a40db8663
jvictort <jvteodorobrasil@gmail.com>
2023-01-25 15:36:50 -0300

    Atualização dos tópicos de Array e For

--- 04-arrays/arrays.c
+++ 04-arrays/arrays.c
@@ -24,5 +24,10 @@ int main()
 
   printf("Sua primeira nota foi %f e você teve a menção %c.\n", notas[0], mencoes[0]);
 
+  // Para criar matrizes a lógica é a mesma com a diferença de que é necessário adicionar colchetes [] de acordo com a dimensão da matriz e especificar o tamanho
+  float notas_mensais[3][2] = {{10.0, 9.0}, {5.0, 4.5}, {8.0, 7.5}};
+
+  printf("A nota do primeiro aluno na primeira prova mensal foi %f.\n", notas_mensais[0][0]);
+
   return 0;
 }
--- 10-for/for.c
+++ 10-for/for.c
@@ -9,8 +9,21 @@ int main()
     que nas outras linguagens de programação.
   */
 
-  for (int i = 1; i <= 5; i++) {
+  for(int i = 1; i <= 5; i++) {
     printf("%d.\n", i);
+  }
+
+  /*
+    Para criar loops aninhados a lógica é a mesma
+  */
+
+  float notas_mensais[3][2] = {{9.5, 9.0}, {5.0, 4.5}, {8.0, 7.5}};
+
+  for(int j = 0; j < 3; j++) {
+    for(int k = 0; k < 2; k++) {
+      printf("| %f |", notas_mensais[j][k]);
+    }
+    printf("\n");
   }
 
   return 0;
