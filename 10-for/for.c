399ca98b3623242df8312a46ffea87602246af40
jvictort <jvteodorobrasil@gmail.com>
2023-01-25 14:07:54 -0300

    For

---
+++ 10-for/for.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  /*
+    O for em C não possui nenhuma diferença notável, se comporta da mesma forma
+    que nas outras linguagens de programação.
+  */
+
+  for (int i = 1; i <= 5; i++) {
+    printf("%d.\n", i);
+  }
+
+  return 0;
+}
