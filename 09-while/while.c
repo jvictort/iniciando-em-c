0a3fd08604bb25375a89d1063f3691845cfc586d
jvictort <jvteodorobrasil@gmail.com>
2023-01-25 14:03:49 -0300

    While e Do While

---
+++ 09-while/while.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  /*
+    O if-else em C não possui nenhuma diferença notável, se comporta da mesma forma
+    que nas outras linguagens de programação.
+  */
+
+  int indice = 1;
+
+  while(indice <= 5) {
+    printf("%d.\n", indice);
+    indice++;
+  }
+
+  do {
+    printf("%d.\n", indice);
+    indice++;
+  } while(indice <= 5);
+
+  return 0;
+}
